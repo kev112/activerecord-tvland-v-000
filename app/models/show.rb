b155b0839a63b7ea82c63a707231a017855effd4
Tao Liu <taoliudesign@gmail.com>
2019-05-26 21:24:29 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++
@@ -1,9 +0,0 @@
-class Show < ActiveRecord::Base
-  has_many :characters
-<<<<<<< HEAD
-  belongs_to :network
-end
-=======
-  has_many :actors, through: :characters
-end
->>>>>>> e97034a2ce48974d1fb701daf170106fa1522441
